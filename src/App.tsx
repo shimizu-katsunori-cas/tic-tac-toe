6a35c6d02abf91ac7ef951ef62df2b31b4e1a941
shimizu-katsunori-cas <katsunori.shimizu@casting-one.jp>
2024-03-25 22:09:54 +0900

    ボタンを押すとプレイヤーが交互に入れ替わるように実装

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import { Button, Stack, Box, Typography } from '@mui/material';
  *
  */
 const App: FC = () => {
+  const [player, setPlayer] = useState('X');
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const GRAY = 'rgba(128, 128, 128, 0.5)';
   return (
     <div>
       <p>
@@ -28,24 +31,105 @@ const App: FC = () => {
       <Stack direction="row">
         <Box>
           <Stack direction="row" spacing={1}>
-            <Button>A</Button>
-            <Button>B</Button>
-            <Button>C</Button>
+            <Button
+              style={{ height: '50px', minWidth: '50px', backgroundColor: GRAY }}
+              onClick={() => {
+              if (board[0] === null) {
+                const newBoard = [...board];
+                newBoard[0] = player;
+                setBoard(newBoard);
+                setPlayer(player === 'X' ? 'O' : 'X');
+              }
+            }}>{board[0]}</Button>
+            <Button
+              style={{ height: '50px', minWidth: '50px', backgroundColor: GRAY }}
+              onClick={() => {
+              if (board[1] === null) {
+                const newBoard = [...board];
+                newBoard[1] = player;
+                setBoard(newBoard);
+                setPlayer(player === 'X' ? 'O' : 'X');
+              }
+            }}>{board[1]}</Button>
+            <Button
+              style={{ height: '50px', minWidth: '50px', backgroundColor: GRAY }}
+              onClick={() => {
+              if (board[2] === null) {
+                const newBoard = [...board];
+                newBoard[2] = player;
+                setBoard(newBoard);
+                setPlayer(player === 'X' ? 'O' : 'X');
+              }
+            }}>{board[2]}</Button>
           </Stack>
           <Stack direction="row" spacing={1}>
-            <Button>D</Button>
-            <Button>E</Button>
-            <Button>F</Button>
+            <Button
+              style={{ height: '50px', minWidth: '50px', backgroundColor: GRAY }}
+              onClick={() => {
+              if (board[3] === null) {
+                const newBoard = [...board];
+                newBoard[3] = player;
+                setBoard(newBoard);
+                setPlayer(player === 'X' ? 'O' : 'X');
+              }
+            }}>{board[3]}</Button>
+            <Button
+              style={{ height: '50px', minWidth: '50px', backgroundColor: GRAY }}
+              onClick={() => {
+              if (board[4] === null) {
+                const newBoard = [...board];
+                newBoard[4] = player;
+                setBoard(newBoard);
+                setPlayer(player === 'X' ? 'O' : 'X');
+              }
+            }}>{board[4]}</Button>
+            <Button
+              style={{ height: '50px', minWidth: '50px', backgroundColor: GRAY }}
+              onClick={() => {
+              if (board[5] === null) {
+                const newBoard = [...board];
+                newBoard[5] = player;
+                setBoard(newBoard);
+                setPlayer(player === 'X' ? 'O' : 'X');
+              }
+            }}>{board[5]}</Button>
           </Stack>
           <Stack direction="row" spacing={1}>
-            <Button>G</Button>
-            <Button>H</Button>
-            <Button>I</Button>
+            <Button
+              style={{ height: '50px', minWidth: '50px', backgroundColor: GRAY }}
+              onClick={() => {
+              if (board[6] === null) {
+                const newBoard = [...board];
+                newBoard[6] = player;
+                setBoard(newBoard);
+                setPlayer(player === 'X' ? 'O' : 'X');
+              }
+            }}>{board[6]}</Button>
+            <Button
+              style={{ height: '50px', minWidth: '50px', backgroundColor: GRAY }}
+              onClick={() => {
+              if (board[7] === null) {
+                const newBoard = [...board];
+                newBoard[7] = player;
+                setBoard(newBoard);
+                setPlayer(player === 'X' ? 'O' : 'X');
+              }
+            }}>{board[7]}</Button>
+            <Button
+              style={{ height: '50px', minWidth: '50px', backgroundColor: GRAY }}
+              onClick={() => {
+              if (board[8] === null) {
+                const newBoard = [...board];
+                newBoard[8] = player;
+                setBoard(newBoard);
+                setPlayer(player === 'X' ? 'O' : 'X');
+              }
+            }}>{board[8]}</Button>
           </Stack>
         </Box>
         <Box>
           <Typography variant="h5" component="h5">
-            hello
+            Next Player: {player}
           </Typography>
           <Button>reset</Button>
         </Box>
